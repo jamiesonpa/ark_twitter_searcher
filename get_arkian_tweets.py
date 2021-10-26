6739787e8fc03104c78e1cd76dd09d3eb1bb1db2
Pierce Jamieson <jamieson.pierce@gmail.com>
2021-10-26 14:52:08 -0500

    Update get_arkian_tweets.py

--- get_arkian_tweets.py
+++ get_arkian_tweets.py
@@ -10,7 +10,7 @@ access_secret = st.secrets["access_secret"]
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tweepy.API(auth)
 
 
 def search_twitter(arkian,searchcriteria,api,rts):
