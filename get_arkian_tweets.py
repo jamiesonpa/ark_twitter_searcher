71f84dad5a3aa4ca4f46d791c81e53faa9a0bd74
Pierce Jamieson <jamieson.pierce@gmail.com>
2021-10-26 16:04:16 -0500

    Update get_arkian_tweets.py

--- get_arkian_tweets.py
+++ get_arkian_tweets.py
@@ -17,43 +17,46 @@ api = tweepy.API(auth)
 
 
 def search_twitter(arkian,searchcriteria,api,rts, start, end):
-    arkian = arkian.replace("@","")
-    # st.write("start date is of type: " + str(type(start)))
-    # st.write("end date is of type: " + str(type(end)))
+    try:
+        arkian = arkian.replace("@","")
+        # st.write("start date is of type: " + str(type(start)))
+        # st.write("end date is of type: " + str(type(end)))
 
-    tweets = tweepy.Cursor(api.user_timeline, screen_name=arkian, tweet_mode="extended", include_rts=rts).items()
-    # tweets = api.user_timeline(screen_name=arkian, 
-    #                         # 200 is the maximum allowed count
-    #                         count=200,
-    #                         include_rts = rts,
-    #                         # Necessary to keep full_text 
-    #                         # otherwise only the first 140 words are extracted
-    #                         tweet_mode = 'extended'
-    #                         )
-    tweets_to_print = []
-    total_tweets = []
-    for info in tweets:
-        total_tweets.append(info)
-        if (info.full_text.lower()).find(searchcriteria.lower()) != -1:
-            created_date = str(info.created_at).split(" ")[0]
-            tweet_date = datetime.strptime(created_date,"%Y-%m-%d").date()
-            #st.write("created date is of type " + str(type(tweet_date)))
-            if start <= tweet_date <= end:
-                tweets_to_print.append(info)
-            else:
-                pass
+        tweets = tweepy.Cursor(api.user_timeline, screen_name=arkian, tweet_mode="extended", include_rts=rts).items()
+        # tweets = api.user_timeline(screen_name=arkian, 
+        #                         # 200 is the maximum allowed count
+        #                         count=200,
+        #                         include_rts = rts,
+        #                         # Necessary to keep full_text 
+        #                         # otherwise only the first 140 words are extracted
+        #                         tweet_mode = 'extended'
+        #                         )
+        tweets_to_print = []
+        total_tweets = []
+        for info in tweets:
+            total_tweets.append(info)
+            if (info.full_text.lower()).find(searchcriteria.lower()) != -1:
+                created_date = str(info.created_at).split(" ")[0]
+                tweet_date = datetime.strptime(created_date,"%Y-%m-%d").date()
+                #st.write("created date is of type " + str(type(tweet_date)))
+                if start <= tweet_date <= end:
+                    tweets_to_print.append(info)
+                else:
+                    pass
 
-            # print("ID: {}".format(info.id))
-            # print(info.created_at)
-            # print(info.full_text)
-            # print("\n")
-    if len(tweets_to_print) == 0:
-        st.write("Retrieved " + str(len(total_tweets)) + " total tweets, and searched them for keyword '" + searchcriteria + "'.")
-        st.write("No tweets with '" + str(searchcriteria) + "' were found in those tweets of @" + arkian)
-    else:
-        st.write("Retrieved " + str(len(total_tweets)) + " total tweets, and searched them for keyword '" + searchcriteria + "'.")
-        for tweet in tweets_to_print:
-            st.write("["+str(tweet.created_at) + "] @" + arkian + ": " + str(tweet.full_text))
+                # print("ID: {}".format(info.id))
+                # print(info.created_at)
+                # print(info.full_text)
+                # print("\n")
+        if len(tweets_to_print) == 0:
+            st.write("Retrieved " + str(len(total_tweets)) + " total tweets, and searched them for keyword '" + searchcriteria + "'.")
+            st.write("No tweets with '" + str(searchcriteria) + "' were found in those tweets of @" + arkian)
+        else:
+            st.write("Retrieved " + str(len(total_tweets)) + " total tweets, and searched them for keyword '" + searchcriteria + "'.")
+            for tweet in tweets_to_print:
+                st.write("["+str(tweet.created_at) + "] @" + arkian + ": " + str(tweet.full_text))
+    except:
+        st.write("Error occurred, please check to make sure that your inputs are good.")
 
 
 st.title("ARK Twitter Searcher v0.1")
